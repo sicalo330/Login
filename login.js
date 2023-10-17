22570033b3cef6305d40cdc8fc949f08ef87f041
sicalo330 <132423242+sicalo330@users.noreply.github.com>
2023-10-17 16:51:21 -0500

    Agregar envío a consola y limpieza de datos

---
+++ login.js
@@ -0,0 +1,12 @@
+const enviar = document.querySelector("#Enviar");
+enviar.addEventListener('click', function(e){
+    e.preventDefault();
+    enviarDatos();
+});
+
+function enviarDatos(){
+    const nombre = document.getElementById("nombre").value;
+    const constrasenia = document.getElementById("contrasenia").value;
+    console.log(nombre);
+    console.log(constrasenia);
+}
